a7bfed748f27a4f693839002ab44b1ce257d769b
jalilmboyzt29 <92901357+jalilmboyzt29@users.noreply.github.com>
2023-02-14 19:10:57 +0900

    Create koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,17 @@
+<?php
+
+$host = "localhost";
+
+$user = "root";
+
+$password = "";
+
+$database = "db_pagination";
+
+$koneksi = mysqli_connect($host, $user, $password, $database);
+
+if (!$koneksi) {
+
+	die("Koneksi gagal: " . mysqli_connect_error());}
+
+?>
